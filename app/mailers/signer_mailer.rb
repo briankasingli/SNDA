974b86ee0a0b7c274b32b8471044e23cd5c26943
Brian Li <brianli@Brians-MacBook-Pro.local>
2015-08-03 02:05:11 -0700

    edited the email, so NDA submissions now goes to the rightfull owner

--- app/mailers/signer_mailer.rb
+++ app/mailers/signer_mailer.rb
@@ -4,29 +4,34 @@ class SignerMailer < ActionMailer::Base
 
   def signed_nda(signed_nda)    
 		generatePDF(signed_nda)
-		mail(:to => 'brian.kasing.li@gmail.com', :subject => "#{signed_nda.full_name}, Here is your signed NDA")
+		mail(to: "#{signed_nda.email}", subject: "#{signed_nda.full_name}, Here is your signed NDA")
   end
 
   private 
 
-  def generatePDF(signed_nda)
-  	pdf = Prawn::Document.new
-		@array_paragraphs = signed_nda.nda.gsub('\n','').gsub('\t', '').gsub('</p><p>', 'CCCCC').gsub('<p>', '').gsub('</p>','').split("CCCCC")
+	  def generatePDF(signed_nda)
+	  	
+	  	pdf = Prawn::Document.new
 
-		@array_paragraphs.each do |paragraph|
-			pdf.text("#{paragraph}")
-			pdf.text("\n")
-		end
+	  	##clean paragraphs
+			@array_paragraphs = signed_nda.nda.gsub('\n','').gsub('\t', '').gsub('</p><p>', 'CCCCC').gsub('<p>', '').gsub('</p>','').split("CCCCC")
 
-		data_url = signed_nda.signture_image
-		png = Base64.decode64(data_url['data:image/png;base64,'.length .. -1])
-		File.open('signature.png', 'wb') { |f| f.write(png) }
+			##render paragraphs
+			@array_paragraphs.each do |paragraph|
+				pdf.text("#{paragraph}")
+				pdf.text("\n")
+			end
 
-		signature = "./signature.png" 
-		pdf.image signature, :at => [40,170], :width => 450
+			##render image
+			data_url = signed_nda.signture_image
+			png = Base64.decode64(data_url['data:image/png;base64,'.length .. -1])
+			File.open('signature.png', 'wb') { |f| f.write(png) }
 
-		attachments["#{signed_nda.full_name}.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render }
-  end
+			signature = "./signature.png" 
+			pdf.image signature, :at => [40,170], :width => 450
+
+			attachments["#{signed_nda.full_name}.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render }
+	  end
 
 end
 
--- app/views/signed_documents/show.pdf.prawn
+++ app/views/signed_documents/show.pdf.prawn
@@ -1,10 +1,13 @@
+##clean paragraphs
 @array_paragraphs = @signed_document.nda.gsub('\n','').gsub('\t', '').gsub('</p><p>', 'CCCCC').gsub('<p>', '').gsub('</p>','').split("CCCCC")
 
+##render paragraphs
 @array_paragraphs.each do |paragraph|
 	pdf.text("#{paragraph}")
 	pdf.text("\n")
 end
 
+##render image
 data_url = @signed_document.signture_image
 png = Base64.decode64(data_url['data:image/png;base64,'.length .. -1])
 File.open('signature.png', 'wb') { |f| f.write(png) }
